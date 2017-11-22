284614ca4ce7fdc20cd265cfbcf060bdb990570c
tnt01 <mhake@torinit.com>
2017-11-22 11:15:23 +0530

    added Bulletin Board, remaining building pages and remaining popups of community

--- js/eServusFront.js
+++ js/eServusFront.js
@@ -82,7 +82,6 @@
 		});
 	 	
 
-
     	//Building Page Slider JS
 		 $('#buildingSlider').lightSlider({
 		        gallery:true,
@@ -141,7 +140,6 @@
             
             $('html, body').animate({scrollTop:toBeScrolledOffset}, 'slow');
 		});
-
 
 	// for Community Page calender JS
 		$(document).ready(function() {
@@ -216,3 +214,32 @@
 			});
 			
 		});
+
+
+	//For Bulletin Board Post Pagination JS
+			YUI().use(
+			  'aui-pagination',
+			  function(Y) {
+			    new Y.Pagination(
+			      {
+			        after: {
+			          changeRequest: function(event) {
+			            console.log(
+			              'page:', event.state.page,
+			              'getOffsetPageNumber:', this.getOffsetPageNumber()
+			            );
+			          }
+			        },
+			        boundingBox: '#bbPostPagination',
+			        offset: 1,
+			        page: 1,
+			        strings: {
+			          next: '>',
+			          prev: '<'
+			        },
+			        total: 5
+			      }
+			    ).render();
+
+			  }
+			);
